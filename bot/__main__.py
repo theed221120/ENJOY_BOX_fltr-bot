ee097e9419785b4bceee53543f1c3322dbc2a501
hsk_the_editor <85099920+theed221120@users.noreply.github.com>
2021-06-13 10:51:04 +0530

    Add files via upload

---
+++ bot/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG and @hsk_the_editor
+
+from .bot import Bot
+
+app = Bot()
+app.run()
---
+++ bot/translation.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG and @hsk_the_editor
+
+class Translation(object):
+    
+    START_TEXT = """<b>ഹലോ {}!!</b>
+<i>ഞാൻ ENJOY BOX മൂവി ഗ്രൂപ്പില്‍ സിനിമ തരുന്ന റോബോട്ടാണ്. സിനിമകള്‍ കിട്ടാൻ Join 👉 @EB_Movies 
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i><u>How To Use Me 🤔 But Only On ENJOY BOX movie group 😁!?</u></i></b>
+
+<i>
+-> Add Me To Any Group And Make Me Admin
+-> Add Me To Your Desired Channel
+</i>
+
+<b>Bot Commands (Works Only In Groups) :</b>
+
+    -> <code>/add chat_id</code>
+                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+     <code>/add @Username</code>
+     
+    -> <code>/del chat_id</code>
+                OR                  - To disconnect A Group With A Channel
+     <code>/del @Username</code>
+     
+    -> <code>/kurinji</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    
+    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+@hsk_the_editor
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> ENJOY BOX Robot</code>
+    
+<b>➥ Re creator</b> : <b><i><a href="https://t.me/hsk_the_editor">hsk the editor</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://t.me/EB_Movies">Don't click here for source code</a></i>
+"""
